092df9e573e80f471b2c76db3b29285eeff7ea92
Papeet2785 <carmeet2785@gmail.com>
2026-08-21 16:23:26 -0400

    adding flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Python + LangChain development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+          pyright
+          ruff
+          python3Packages.python-lsp-server
+          python3Packages.python-dotenv
+          python3Packages.pydantic
+          python3Packages.langchain
+          python3Packages.langchain-openai
+        ];
+      };
+    };
+}
